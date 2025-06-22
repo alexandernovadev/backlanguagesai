80c357f7b20f3fc70cf4bf14998981a32ac87fd3
alexandernovadev <alexsk88.dev@gmail.com>
2025-06-22 15:45:22 -0500

    fix: update CORS origin from localhost:3000 to localhost:5173 for development

--- src/main.ts
+++ src/main.ts
@@ -36,7 +36,7 @@ app.use(express.json());
 app.use(
   cors({
     origin: [
-      "http://localhost:3000",
+      "http://localhost:5173",
       "https://languages-ai.alexandernova.pro",
     ],
     credentials: true,

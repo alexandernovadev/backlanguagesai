d747aab594ca2b11882626a8f9ab62598ea19d92
alexandernovadev <alexsk88.dev@gmail.com>
2025-06-30 20:46:53 -0500

    refactor: improve Markdown generation instructions in text stream service
    - Revised the Markdown generation instructions for clarity and adherence to ISO 639-1 standards.
    - Enhanced educational purpose description and formatting guidelines for better comprehension.
    - Standardized language usage in the content guidelines to ensure consistency.

--- src/app/services/ai/generateTextStream.ts
+++ src/app/services/ai/generateTextStream.ts
@@ -30,8 +30,8 @@ export const generateTextStreamService = async ({
       {
         role: "system",
         content: `
-          Generate a Markdown text in language "${language}" according with ( ISO 639-1 para los idiomas ) , 
-          this text is for an educational purpose with a "${typeWrite}" style, using vocabulary and 
+          Generate a Markdown text in language "${language}" according to ISO 639-1 standards for languages. 
+          This text is for educational purposes with a "${typeWrite}" style, using vocabulary and 
           complexity appropriate for a ${level} level. 
 
           Formatting guidelines:
@@ -40,8 +40,8 @@ export const generateTextStreamService = async ({
           - Use additional subtitles as "### Subtitle" if needed.
           - Avoid using additional H1 or H2 headers after the initial title and subtitle.
           - Don't use h4 or h5 or h6 headers.
-          - Don't USE old fashioned words
-          - Don’t use nested lists (sub-lists) in the Markdown.
+          - Don't use old fashioned words
+          - Don't use nested lists (sub-lists) in the Markdown.
 
           Content guidelines:
           - Avoid using rare, uncommon words or special symbols, as each word should be clickable and easily searchable in a dictionary.
